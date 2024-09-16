621f3ec7ae20c04c0172277e1e2c32091d3a14ab
Caio Ramalho <103006310+RamalhoCaio@users.noreply.github.com>
2024-09-16 08:31:17 -0300

    Create Arara.java

---
+++ 04. Polimorfismo/Arara.java
@@ -0,0 +1,7 @@
+
+class Arara extends Animal {
+    @Override //polimorfismo, onde a classe filha sobrescreve o método da classe pai
+    public void fazerSom() {
+        System.out.println("Pru Pru");
+    }
+}
