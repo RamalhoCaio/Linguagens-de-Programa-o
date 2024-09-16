9c898ea53409e164097444df83b2bec1ee8ef77c
Caio Ramalho <103006310+RamalhoCaio@users.noreply.github.com>
2024-09-16 08:31:04 -0300

    Create Cobra.java

---
+++ 04. Polimorfismo/Cobra.java
@@ -0,0 +1,6 @@
+class Cobra extends Animal {
+    @Override //polimorfismo, onde a classe filha sobrescreve o método da classe pai
+    public void fazerSom() {
+        System.out.println("Sssssssssssss!");
+    }
+}
