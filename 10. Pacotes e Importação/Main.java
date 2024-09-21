5342db6fcda381145cda8a21066cfb7f0c0fa91a
Enrico Modena <102961468+enricofm@users.noreply.github.com>
2024-09-21 15:22:02 -0300

    Update Main.java

--- 10. Pacotes e Importação/Main.java
+++ 10. Pacotes e Importação/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner; // importa a classe Scanner do pacote java.util
 class Main {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
-    String userName;
+    String username;
     
     // Digitar o nome de usuario
     System.out.println("Digite o nome de usuario:"); 
-    userName = myObj.nextLine();   
+    username = myObj.nextLine();   
        
-    System.out.println("Username is: " + userName);        
+    System.out.println("Nome de usuario: " + username);        
   }
 }
